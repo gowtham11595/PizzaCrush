6fe42f910d25b39d9768374ae52269b8b4405a95
Gowtham Kesa <gowtham11595@gmail.com>
2019-10-21 23:44:12 -0500

    deletion of branches, bread, pizza etc

--- code/application/modules/Login/views/deletebranch.php
+++ code/application/modules/Login/views/deletebranch.php
@@ -13,17 +13,17 @@ else{e.value="no";location.reload();}
 }</script></head>
 <body style="background:url('http://images7.alphacoders.com/328/328730.jpg')">
 <?php
-$data=$this->db->select('branch_name')->from('branches')->where('status','added')->get()->result();
+$data=$this->db->select('city')->from('branches')->where('status','added')->get()->result();
 ?>
 
 <form action="<?php echo base_url();?>deletebranch_submit" method="post">
-Branch Name: 
-<select name="branch_name">
+Branch Name:
+<select name="city">
 <?php
 foreach($data as $key=>$value)
 {
 	$value=(array)$value;
-	$value=$value['branch_name'];
+	$value=$value['city'];
 ?>
     <option value="<?php print_r($value); ?>">
 	<?php print_r($value); ?>
@@ -32,20 +32,9 @@ foreach($data as $key=>$value)
 }
 ?>
 </select>
-Select State:
-Select the state:
-<select name="state">
-<option value="Andhra Pradesh">Andhra Pradesh</option>
-<option value="Arunachal Pradesh">Arunachal Pradesh</option>
-<option value="Assam">Assam</option>
-<option value="Bihar">Bihar</option>
-<option value="Chhattisgarh">Chhattisgarh</option>
-<option value="Kerala">Kerala</option>
-<option value="Karnataka">Karnataka</option>
-<option value="Maharastra">Maharastra</option>
-<option value="TamilNadu">TamilNadu</option>
-<option value="Telangana">Telangana</option>
-</select>
+
+
+
 
 <input type="submit">
 </form>
