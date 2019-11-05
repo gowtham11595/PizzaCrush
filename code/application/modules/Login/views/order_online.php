c36bdb422ae903d16585b2bb8fedde6186f48fc6
Souab Reddy Pailla <sourabreddyp@gmail.com>
2019-11-04 20:46:57 -0600

    Code for ordering and display toppings

--- code/application/modules/Login/views/order_online.php
+++ code/application/modules/Login/views/order_online.php
@@ -14,22 +14,22 @@ else{e.value="no";location.reload();}
 <?php 			    if($this->session->userdata('admin')==TRUE){?>
 <a href="<?php echo base_url();?>afteradminlogin"><button type="button" class="navbar-btn btn-success btn pull-left" style="margin-right:30px;">Home</button>
 				<?php } else{?>
-				
+
 				<a href=""><button type="button" class="navbar-btn btn-success btn pull-left" style="margin-right:30px;">Home<button/></a>
 				<?php }?>
-<a href="<?php echo base_url();?>locator"><button type="button" class="navbar-btn btn-success btn pull-left">Restaurant Locator</button></a>      
- <a href="changeloc" class="navbar-btn btn-primary btn pull-left" style="margin-left:30px;">Change my details</a>       
+<a href="<?php echo base_url();?>locator"><button type="button" class="navbar-btn btn-success btn pull-left">Restaurant Locator</button></a>
+ <a href="changeloc" class="navbar-btn btn-primary btn pull-left" style="margin-left:30px;">Change my details</a>
 <?php
-if($this->session->userdata('id')!=null){ 
+if($this->session->userdata('id')!=null){
 ?>
 <a href="<?php echo base_url();?>logout" class="navbar-btn btn-danger btn pull-right" style="  margin-left:30px;margin-right:150px;">Logout</a>
 <?php
 }
 ?>
 </head>
-      
+
 <a href="readymade" class="navbar-btn btn-primary btn pull-left" style="margin-left:30px;">Order pizzas from menu</a>
-<a href="ownpizza" class="navbar-btn btn-primary btn pull-left" style="margin-left:30px;">Make my own pizza</a>
+<a href="ownpizza" class="navbar-btn btn-primary btn pull-left" style="margin-left:30px;" disabled>Customize my own pizza</a>
 <body style="background:url('http://images7.alphacoders.com/328/328730.jpg')">
 
 
